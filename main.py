aaa5574696330dd8917710797ff5b563672be3bb
tiniuspre <70323207+tiniuspre@users.noreply.github.com>
2021-12-20 15:36:25 +0100

    main.py Henter alle crestron produkter i ett domotz nettverk

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ from json import load
 
 config = load(open('config.json'))
 
-crestron = EasyCrestron(ip="10.0.11.181", brukernavn="admin", passord="admin")
-domotz = Domotz(api_key=config['api_key'], api_url=config['api_url'])
+id = 0
+agent_valg = "CB6"
 
+
+def getCrestron():
+    domotz = Domotz(api_key=config['api_key'], api_url=config['api_url'])
+    for agent in domotz.agents():
+        if agent['NAVN'] == agent_valg:
+            domotz.select(agent['ID'])
+
+    enheter = {}
+    for enhet in domotz.devices():
+        if enhet['vendor'] == 'CRESTRON ELECTRONICS, INC.':
+            enheter.update({enhet['id']: {'ip': enhet['ip_addresses'][0], 'navn': enhet['names']['host'], 'id': enhet['id'],
+                             'model': enhet['model']}})
+
+    return enheter
